871ed285f5bd25956ecd3d3cbe997cd14e0fbafb
VladislavMolodtsov <vladi28061750@gmail.com>
2023-05-27 15:56:34 +0300

    added several comments to lesson

--- my-todo/src/components/app/app.js
+++ my-todo/src/components/app/app.js
@@ -6,18 +6,23 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
+// В консоли записываем npm i - у нас загрузятся все зависимости в package.json и установится папка node_modules
+// Подключаем готовые стили, готовые иконки с cdn.js сервера bootstrap.min.css, font-awesome/all.min.css
+// Cоздаем название папок через "-" и с маленькой буквы
+// Имена компонентам задаем с большой буквы, если название состоит из нескольких слов, то используем CamelCase
+
 function App() {
         return (
                 <div className='app'>
-                        <AppInfo/>
+                        <AppInfo/> {/* Общая информация о нашем приложении */}
 
                         <div className="search-panel">
-                                <SearchPanel/>
-                                <AppFilter/>
+                                <SearchPanel/> {/* Строка поиска */}
+                                <AppFilter/> {/* Фильтры */}
                         </div>
 
-                        <EmployersList/>
-                        <EmployersAddForm/>
+                        <EmployersList/> {/* Список сотрудников, каждый отдельный сотрудник - это отдельный компонент EmployersListItem */}
+                        <EmployersAddForm/> {/* Компонент с добавлением каждого нового сотрудника */}
                 </div>
         );
 }
