e44c3004232f691fec7e4b5926b54f2cefe31c80
VladislavMolodtsov <vladi28061750@gmail.com>
2023-06-19 21:37:11 +0300

    use state in project (increase, rise)

--- my-todo/src/components/employers-list-item/employers-list-item.js
+++ my-todo/src/components/employers-list-item/employers-list-item.js
@@ -1,35 +1,65 @@
+import { Component } from 'react';
 import './employers-list-item.css';
 
-const EmployersListItem = ({name, salary, increase}) => {
-
-        let classs = 'list-group-item d-flex justify-content-between ';
-
-        if (increase) {
-                classs += 'increase';
+class EmployersListItem extends Component {
+        constructor(props) {
+                super(props);
+                this.state = {
+                        increase: false,
+                        rise: false
+                }
         }
 
-        return (
-                <li className={classs}>
-                        <span className="list-group-item-label">{name}</span>
-                        <input
-                                type="text"
-                                className="list-group-item-input"
-                                defaultValue={salary + '$'} />
+        onIncrease = () => {
+                this.setState(({increase}) => ({
+                        increase: !increase
+                }))
+        }
+        onRise = () => {
+                this.setState(({rise}) => ({
+                        rise: !rise
+                }))
+        }
 
-                        <div className='d-flex justify-content-center align-items-center'>
-                                <button type="button"
-                                        className="btn-cookie btn-sm ">
-                                        <i className="fas fa-cookie"></i>
-                                </button>
 
-                                <button type="button"
-                                        className="btn-trash btn-sm ">
-                                        <i className="fas fa-trash"></i>
-                                </button>
-                                <i className="fas fa-star"></i>
-                        </div>
-                </li>
-        )
+        render() {
+                const {name, salary} = this.props;
+                const {increase, rise} = this.state;
+                
+                let className = 'list-group-item d-flex justify-content-between ';
+                if (increase) {
+                        className += 'increase';
+                }
+                if (rise) {
+                        className += 'like';
+                }
+
+                return (
+                        <li className={className}>
+                                <span 
+                                        className="list-group-item-label"
+                                        onClick={this.onIncrease}>{name}</span>
+                                <input
+                                        type="text"
+                                        className="list-group-item-input"
+                                        defaultValue={salary + '$'} />
+
+                                <div className='d-flex justify-content-center align-items-center'>
+                                        <button type="button"
+                                                className="btn-cookie btn-sm"
+                                                onClick={this.onRise}>
+                                                <i className="fas fa-cookie"></i>
+                                        </button>
+
+                                        <button type="button"
+                                                className="btn-trash btn-sm ">
+                                                <i className="fas fa-trash"></i>
+                                        </button>
+                                        <i className="fas fa-star"></i>
+                                </div>
+                        </li>
+                )
+        }     
 }
 
 export default EmployersListItem;
