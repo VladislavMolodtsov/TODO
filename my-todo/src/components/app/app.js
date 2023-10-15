44a7fcb8b6fb8e323e4c7400f6e4cc054ba6b135
VladislavMolodtsov <vladi28061750@gmail.com>
2023-10-15 15:09:32 +0300

    component SearchPanel shapes its own state with component App. And it already filter the list through the method serchEmp

--- my-todo/src/components/app/app.js
+++ my-todo/src/components/app/app.js
@@ -10,74 +10,87 @@ import './app.css';
 
 class App extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [
-				{ name: 'John C.', salary: 800, increase: false, rise: false, id: 1 },
-				{ name: 'Alex M.', salary: 3000, increase: false, rise: false, id: 2 },
-				{ name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3 }
-			]
-		}
-		this.maxId = 4;
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: 'John C.', salary: 800, increase: false, rise: false, id: 1 },
+                { name: 'Alex M.', salary: 3000, increase: false, rise: false, id: 2 },
+                { name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3 }
+            ],
+            term: ''
+        }
+        this.maxId = 4;
+    }
 
-	deleteItem = (id) => {
-		this.setState(({ data }) => {
-			return {
-				data: data.filter(item => item.id !== id)
-			}
-		})
-	}
+    deleteItem = (id) => {
+        this.setState(({ data }) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-	addItem = (name, salary) => {
-		const newItem = {
-			name,
-			salary,
-			increase: false,
-			id: this.maxId++
-		}
-		this.setState(({ data }) => {
-			const newArr = [...data, newItem];
-			return {
-				data: newArr
-			}
-		});
-	}
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({ data }) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
 
-	onToggleProp = (id, prop) => {
-		this.setState(({ data }) => ({
-			data: data.map(item => {
-				if (item.id === id) {
-					return { ...item, [prop]: !item[prop] }
-				}
-				return item
-			})
-		}))
-	}
+    onToggleProp = (id, prop) => {
+        this.setState(({ data }) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return { ...item, [prop]: !item[prop] }
+                }
+                return item
+            })
+        }))
+    }
 
-	render() {
-		const { data } = this.state;
-		const employers = data.length;
-		const increase = data.filter(item => item.increase).length;
+    onUpdateTerm = (term) => {
+        this.setState({term})
+    }
 
-		return (
-			<div className='app'>
-				<AppInfo employers={employers} increase={increase}/>
+    searchEmp = (data, term) => {
+        if (term) {
+            return data.filter(item => item.name.indexOf(term) > -1);
+        }
+        return data;
+    }
 
-				<div className="search-panel">
-					<SearchPanel />
-					<AppFilter />
-				</div>
+    render() {
+        const { data, term } = this.state;
+        const employers = data.length;
+        const increase = data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
 
-				<EmployersList
-					data={data}
-					onDelete={this.deleteItem}
-					onToggle={this.onToggleProp} />
-				<EmployersAddForm onAdd={this.addItem} />
-			</div>
-		);
-	}
+        return (
+            <div className='app'>
+                <AppInfo employers={employers} increase={increase} />
+
+                <div className="search-panel">
+                    <SearchPanel onUpdateTerm={this.onUpdateTerm}/>
+                    <AppFilter />
+                </div>
+
+                <EmployersList
+                    data={visibleData}
+                    onDelete={this.deleteItem}
+                    onToggle={this.onToggleProp} />
+                <EmployersAddForm onAdd={this.addItem} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- my-todo/src/components/search-panel/search-panel.js
+++ my-todo/src/components/search-panel/search-panel.js
@@ -1,12 +1,35 @@
+import { Component } from 'react';
+
 import './search-panel.css';
 
-const SearchPanel = () => {
+class SearchPanel extends Component {
+    constructor() {
+        super();
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdateTerm = (e) => {
+        const term = e.target.value;
+        this.setState({term})
+        this.props.onUpdateTerm(term)
+    }
+
+    render() {
+        const {term} = this.state;
+
         return (
-                <input 
-                        type="text"
-                        className='form-control search-input'
-                        placeholder='Найти сотрудника'/>
+            <>
+                <input
+                    type = "text"
+                    className = 'form-control search-input'
+                    placeholder = 'Найти сотрудника' 
+                    value={term}
+                    onChange={this.onUpdateTerm} />
+            </>
         )
+    }
 }
 
 export default SearchPanel;
